d8270e34072439691b21a5b6e5d8691c9950fb18
phAlbanoMello <phmello8@gmail.com>
2026-05-07 17:38:34 -0300

    Drawing a triangle in a TGA texture

--- main.cpp
+++ main.cpp
@@ -7,14 +7,63 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
+    
+    bool steepLine = std::abs(ax - bx) < std::abs(ay - by);
+    
+    if (steepLine)//if its a steep line, transpose the image
+    {
+        std::swap(ax, ay);
+        std::swap(bx, by);
+    }
+    
+    if (ax > bx) //make it left to right
+    {
+        std::swap(ax, bx);
+        std::swap(ay, by);
+    }
+    
+    for (int x = ax; x <= bx; x++)
+    {
+        //third attempt
+        float t = (x - ax) / static_cast<float>(bx - ax);
+        int y = std::round(ay + (by - ay) * t);
+        if (steepLine)
+        {
+            //de-transpose
+            framebuffer.set(y, x, color);
+        }
+        else
+        {
+            framebuffer.set(x, y, color);
+        }
+        
+        
+        //second attempt
+        /*float t = (x - ax) / static_cast<float>(bx - ax);
+        int y = std::round(ay + (by - ay) * t);
+        framebuffer.set(x, y, color);*/
+
+        //first attempt
+        /*int x = std::round(ax + (bx - ax) * t);
+        int y = std::round(ay + (by - ay) * t);
+        framebuffer.set(x, y, color);*/
+    }
+}
+
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
     TGAImage framebuffer(width, height, TGAImage::RGB);
 
-    int ax =  7, ay =  3;
-    int bx = 12, by = 37;
-    int cx = 62, cy = 53;
+    int ax =  7, ay =  15;
+    int bx = 10, by = 37;
+    int cx = 47, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
