47cffcb377ad6eb12504bbcab53a960a7eaec7a8
phAlbanoMello <phmello8@gmail.com>
2026-05-27 14:28:55 -0300

    My approach to loading the wavefront model
    - Added model class
    - Removed model loading and drawing functionality from main

--- main.cpp
+++ main.cpp
@@ -1,75 +1,14 @@
-#include <cmath>
-#include "tgaimage.h"
+#include <string>
+#include "build/model.h"
 
-constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
-constexpr TGAColor green   = {  0, 255,   0, 255};
-constexpr TGAColor red     = {  0,   0, 255, 255};
-constexpr TGAColor blue    = {255, 128,  64, 255};
-constexpr TGAColor yellow  = {  0, 200, 255, 255};
-
-void line(int ax, int ay, int bx, int by, TGAImage& framebuffer, TGAColor color) {
-    
-    bool steepLine = std::abs(ax - bx) < std::abs(ay - by);
-    
-    if (steepLine)//if its a steep line, transpose the image
-    {
-        std::swap(ax, ay);
-        std::swap(bx, by);
-    }
-    
-    if (ax > bx) //make it left to right
-    {
-        std::swap(ax, bx);
-        std::swap(ay, by);
-    }
-    
-    for (int x = ax; x <= bx; x++)
-    {
-        //third attempt
-        float t = (x - ax) / static_cast<float>(bx - ax);
-        int y = std::round(ay + (by - ay) * t);
-        if (steepLine)
-        {
-            //de-transpose
-            framebuffer.set(y, x, color);
-        }
-        else
-        {
-            framebuffer.set(x, y, color);
-        }
-        
-        
-        //second attempt
-        /*float t = (x - ax) / static_cast<float>(bx - ax);
-        int y = std::round(ay + (by - ay) * t);
-        framebuffer.set(x, y, color);*/
-
-        //first attempt
-        /*int x = std::round(ax + (bx - ax) * t);
-        int y = std::round(ay + (by - ay) * t);
-        framebuffer.set(x, y, color);*/
-    }
-}
+const std::string objPath = "C:/Repos/myTinyRenderer/obj/diablo3_pose/diablo3_pose.obj";
 
 int main(int argc, char** argv) {
-    constexpr int width  = 64;
-    constexpr int height = 64;
-    TGAImage framebuffer(width, height, TGAImage::RGB);
+    constexpr int width  = 1920;
+    constexpr int height = 1920;
 
-    int ax =  7, ay =  15;
-    int bx = 10, by = 37;
-    int cx = 47, cy = 53;
+    Model model(objPath);
+    model.DrawModelToFile(width, height);
 
-    line(ax, ay, bx, by, framebuffer, blue);
-    line(cx, cy, bx, by, framebuffer, green);
-    line(cx, cy, ax, ay, framebuffer, yellow);
-    line(ax, ay, cx, cy, framebuffer, red);
-
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
-
-    framebuffer.write_tga_file("framebuffer.tga");
     return 0;
 }
-
